e845b673f329725d70f958d0998f11179fc7e4a2
Lawrence Tray <lawrence.tray@softwire.com>
2021-09-09 14:04:16 +0100

    Implement progress bar iteration

--- src/view/QuizView.tsx
+++ src/view/QuizView.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Quiz } from "model/Quiz";
 import { QuestionState } from "model/QuestionState";
 import { ProgressBar } from "view/ProgressBar";
@@ -8,17 +8,34 @@ interface QuizViewProps {
 }
 
 export const QuizView = ({ quiz }: QuizViewProps): React.ReactElement => {
-  return (
-    <p>
-      <ProgressBar
-        questionStates={[
-          QuestionState.WRONG,
-          QuestionState.CORRECT,
-          QuestionState.UNANSWERED,
-        ]}
-      />
+  const quizLength = quiz.questions.length;
+  const initialQuestionStates = new Array(quizLength).fill(
+    QuestionState.UNANSWERED
+  );
+  const [questionStates, setQuestionStates] = useState<QuestionState[]>(
+    initialQuestionStates
+  );
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
 
-      {JSON.stringify(quiz)}
-    </p>
+  const updateResult = (correct: boolean): void => {
+    if (currentIndex >= quizLength) {
+      alert("Quiz complete");
+      return;
+    }
+    let newQuestionStates = [...questionStates];
+    newQuestionStates[currentIndex] = correct
+      ? QuestionState.CORRECT
+      : QuestionState.WRONG;
+    setQuestionStates(newQuestionStates);
+    setCurrentIndex(currentIndex + 1);
+  };
+
+  return (
+    <>
+      <ProgressBar questionStates={questionStates} />
+      <button onClick={() => updateResult(Math.random() < 0.5)}>
+        Next Question{" "}
+      </button>
+    </>
   );
 };
