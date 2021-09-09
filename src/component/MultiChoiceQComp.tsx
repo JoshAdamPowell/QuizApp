fe34c275825c16d8f2ab0edb64ce33c9fd6dadf6
Olly Larkin <olly.larkin@softwire.com>
2021-09-09 11:52:12 +0100

    Formatting for the question

--- src/component/MultiChoiceQComp.tsx
+++ src/component/MultiChoiceQComp.tsx
@@ -1,17 +1,32 @@
 import React from "react";
 import { Question } from "model/Question";
 import { AnswersComp } from "component/AnswersComp";
+import styled from "styled-components";
 
 interface MultiChoiceQProps {
   question: Question;
 }
 
+const MultiChoiceQContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+`;
+
+const StyledQuestionImg = styled.img`
+  max-height: 300px;
+`;
+
+const QuestionContainer = styled.div`
+  text-align: center;
+`;
+
 export const MultiChoiceQComp = ({
   question,
 }: MultiChoiceQProps): React.ReactElement => (
-  <div>
-    {question.image && <img src={question.image} />}
-    <div>{question.text}</div>
+  <MultiChoiceQContainer>
+    {question.image && <StyledQuestionImg src={question.image} />}
+    <QuestionContainer>{question.text}</QuestionContainer>
     <AnswersComp answers={question.answers} />
-  </div>
+  </MultiChoiceQContainer>
 );
